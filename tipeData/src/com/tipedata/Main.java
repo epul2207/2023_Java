416ee6878447cb2aa18ce412d87d6b0c8f06d96a
epul2207 <epuls073@gmail.com>
2023-11-17 21:00:00 +0700

    Belajar Java

---
+++ tipeData/src/com/tipedata/Main.java
@@ -0,0 +1,82 @@
+package com.tipedata;
+
+public class Main {
+
+    public static void main(String [] args){
+
+        System.out.println("Mengenal Tipe Data");
+        System.out.println("===INTEGER===");
+        int a = 10;
+        System.out.println("Nilai a adalah : " + a);
+        System.out.println("Nilai Max Integer : " + Integer.MAX_VALUE);
+        System.out.println("Nilai Min Integer : " + Integer.MIN_VALUE);
+        System.out.println("Besar Memory dari Integer : " + Integer.BYTES);
+        System.out.println("Besaran Bit dari Integer : " + Integer.SIZE);
+        System.out.println("############################################");
+        System.out.println(" ");
+        System.out.println("===LONG===");
+        long b = 100L;
+        System.out.println("Nilai a adalah : " + b);
+        System.out.println("Nilai Max long : " + Long.MAX_VALUE);
+        System.out.println("Nilai Min long : " + Long.MIN_VALUE);
+        System.out.println("Besar Memory dari long : " + Long.BYTES);
+        System.out.println("Besaran Bit dari long : " + Long.SIZE);
+        System.out.println("############################################");
+        System.out.println(" ");
+        System.out.println("===SHORT===");
+        short c = 67;
+        System.out.println("Nilai a adalah : " + c);
+        System.out.println("Nilai Max short : " + Short.MAX_VALUE);
+        System.out.println("Nilai Min short : " + Short.MIN_VALUE);
+        System.out.println("Besar Memory dari short : " + Short.BYTES);
+        System.out.println("Besaran Bit dari short : " + Short.SIZE);
+        System.out.println("############################################");
+        System.out.println(" ");
+        System.out.println("===BYTE===");
+        byte d = 7;
+        System.out.println("Nilai a adalah : " + d);
+        System.out.println("Nilai Max byte : " + Byte.MAX_VALUE);
+        System.out.println("Nilai Min byte : " + Byte.MIN_VALUE);
+        System.out.println("Besar Memory dari byte : " + Byte.BYTES);
+        System.out.println("Besaran Bit dari byte : " + Byte.SIZE);
+        System.out.println("############################################");
+        System.out.println(" ");
+        System.out.println("===FLOAT===");
+        float e = 19.8f;
+        System.out.println("Nilai a adalah : " + e);
+        System.out.println("Nilai Max float : " + Float.MAX_VALUE);
+        System.out.println("Nilai Min float : " + Float.MIN_VALUE);
+        System.out.println("Besar Memory dari float : " + Float.BYTES);
+        System.out.println("Besaran Bit dari float : " + Float.SIZE);
+        System.out.println("############################################");
+        System.out.println(" ");
+        System.out.println("===DOUBLE===");
+        double f = 10.988;
+        System.out.println("Nilai a adalah : " + f);
+        System.out.println("Nilai Max double : " + Double.MAX_VALUE);
+        System.out.println("Nilai Min double : " + Double.MIN_VALUE);
+        System.out.println("Besar Memory dari double : " + Double.BYTES);
+        System.out.println("Besaran Bit dari double : " + Double.SIZE);
+        System.out.println("############################################");
+        System.out.println(" ");
+        System.out.println("===CHAR===");
+        char g = 'G';
+        System.out.println("Nilai a adalah : " + g);
+        System.out.println("Nilai Max long : " + Character.MAX_VALUE);
+        System.out.println("Nilai Min long : " + Character.MIN_VALUE);
+        System.out.println("Besar Memory dari long : " + Character.BYTES);
+        System.out.println("Besaran Bit dari long : " + Character.SIZE);
+        System.out.println("############################################");
+        System.out.println(" ");
+        System.out.println("===BOOLEAN===");
+        boolean h = false;
+        System.out.println("Nilai a adalah : " + h);
+//        System.out.println("Nilai Max long : " + Long.MAX_VALUE);
+//        System.out.println("Nilai Min long : " + Long.MIN_VALUE);
+//        System.out.println("Besar Memory dari long : " + Boolean.BYTES);
+//        System.out.println("Besaran Bit dari long : " + Boolean.SIZE);
+        System.out.println("############################################");
+
+
+    }
+}
